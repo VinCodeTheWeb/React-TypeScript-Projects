3bc33c07ec3346e5e2adfc2119f5985a40f61d6c
Vincenzo Pellegrini <vincenzo33.pellegrini@gmail.com>
2021-02-15 23:49:55 +0100

    wiring up redux and created RepositoriesList component

--- redux-ts/src/App.tsx
+++ redux-ts/src/App.tsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 
-import logo from './logo.svg';
-import { store } from './redux/store';
-
+import { RepositoriesList } from './components/repositories-list.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Search For a Package</h1>
+        <RepositoriesList />
       </header>
     </div>
   );
---
+++ redux-ts/src/components/repositories-list.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+const RepositoriesList: React.FC = () => (
+  <div>
+    <form action="">
+      <input type="text" />
+      <button>Search</button>
+    </form>
+  </div>
+)
+
+export { RepositoriesList };
