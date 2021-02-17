30715a4912379c1c099699d882a22a498ecd0abc
Vincenzo Pellegrini <vincenzo33.pellegrini@gmail.com>
2021-02-17 23:53:31 +0100

    adding esbuilt for transformation of input

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,6 +1,44 @@
+import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const App: React.FC = () => <h1>Hi!</h1>
+
+const App: React.FC = () => {
+  const ref = useRef<any>(null);
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+    
+  }
+
+  const onClick = async () => {
+    if (!ref.current) return;
+    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    setCode(result.code);
+  }
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  return (
+    <div>
+      <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
