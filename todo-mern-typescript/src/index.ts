fb1a3474a97df14e1a66f73e4ee7aaa285680550
Vincenzo Pellegrini <vincenzo33.pellegrini@gmail.com>
2021-02-18 23:57:45 +0100

    todo-ts app

---
+++ todo-mern-typescript/src/index.ts
@@ -0,0 +1,13 @@
+import express, { Express } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import todoRoutes from "./routes";
+
+const app: Express = express();
+
+const PORT: string | number = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(todoRoutes);
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
